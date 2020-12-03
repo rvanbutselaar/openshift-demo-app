337db0f3c779c9e123fc42fb23407d61175e27f4
Ronald van Butselaar <ronald@vbutselaar.nl>
2020-12-03 16:59:18 +0100

    clean

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-
         if self.path == "/health":
             self.send_response(200)
             self.end_headers()
