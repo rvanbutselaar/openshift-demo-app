3c6fcc1ffc5afefc8d885a19d69fac97dc4603e4
Ronald van Butselaar <ronald@vbutselaar.nl>
2020-12-03 16:57:58 +0100

    add memory & cpu leak examples

--- app.py
+++ app.py
@@ -5,10 +5,33 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write("Hello {} \n".format(os.environ.get('NAME', 'world!')).encode('utf-8'))
 
+        if self.path == "/health":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+        elif self.path == "/cpuleak":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
 
-httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+            while True:
+                with open('small_file.txt', 'r') as f:
+                    for line in f:
+                        giant_string = ''
+                        giant_string+=line
+
+        elif self.path == "/memoryleak":
+            giant_string = ''
+            while True:
+                with open('small_file.txt', 'r') as f:
+                    for line in f:
+                        giant_string+=line
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write("Hello {} \n".format(os.environ.get('NAME', 'world!')).encode('utf-8'))
+
+if __name__ == '__main__':
+    httpd = HTTPServer(('', 8080), SimpleHTTPRequestHandler)
+    httpd.serve_forever()
